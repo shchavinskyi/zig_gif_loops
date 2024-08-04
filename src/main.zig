26d3b4899cdf4ea1802ca1084ebb0dc7caa5ed17
Oleksandr Shchavinskyi <o.shchavinskyi@gmail.com>
2024-08-04 13:31:04 +0100

    Added initial zig files.

---
+++ src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const mach = @import("mach");
+
+// The global list of Mach modules registered for use in our application.
+pub const modules = .{
+    mach.Core,
+    @import("game_loop.zig"),
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var app = try mach.App.init(allocator, .app);
+    defer app.deinit(allocator);
+    try app.run(.{ .allocator = allocator });
+}
